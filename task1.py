0aab317a38318767b3ca87bd4cb937a5fa9fdbe4
maksimb <maksim.barabanov2002@mail.ru>
2022-06-02 20:16:50 +0500

    Lesson-10 update

--- task1.py
+++ task1.py
@@ -1,21 +1,32 @@
 class Matrix:
-    def __init__(self, list_of_lines: list):
+    def __init__(self, list_of_lines):
         self.list_of_lines = list_of_lines
-        self.number_line = len(self.list_of_lines)
-
-    def __str__(self):
-        return f'{self.list_of_lines[0]}\n{self.list_of_lines[1]}\n{self.list_of_lines[2]}'
 
     def __add__(self, other):
-        new_matrix = [[], [], []]
-        for i in range(3):
-            for j in range(3):
+        new_matrix = []
+        if len(self.list_of_lines) != len(other.list_of_lines):
+            print('Неверная матрица: разная размерность')
+            exit(0)
+        for i in range(len(self.list_of_lines)):
+            new_matrix.append([])
+            for j in range(len(self.list_of_lines)):
                 new_matrix[i].append(self.list_of_lines[i][j] + other.list_of_lines[i][j])
-        return f'{new_matrix[0]}\n{new_matrix[1]}\n{new_matrix[2]}'
+        return Matrix(new_matrix)
+
+    def __str__(self):
+        result = ''
+        for i in range(len(self.list_of_lines)):
+            result += f'{self.list_of_lines[i]}\n'.replace('[', '|').replace(']', '|').replace(',', '')
+        return result
 
 
-first = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-second = Matrix([[10, 11, 12], [13, 14, 15], [16, 17, 18]])
-print(f'first matrix:\n{first}\n')
-print(f'second matrix: \n{second}\n')
-print(f'result matrix: \n{first + second}')
+ex = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+ex1 = Matrix([[7, 1, 5], [2, 5, 8], [1, 11, 10]])
+ex2 = Matrix([[1, 2], [4, 5]])
+ex3 = Matrix([[3, 2], [1, 8]])
+
+a = ex + ex1
+print(a)
+print(a + ex)
+print(ex2 + ex3)
+error = ex + ex3
